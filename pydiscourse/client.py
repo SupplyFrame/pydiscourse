f2e77175f5a3a3c3483fb6c011456c623cbe13a5
Erin Kosewic <devilsavocado10@gmail.com>
2020-08-13 13:22:47 -0700

    fix variable for users call

--- pydiscourse/client.py
+++ pydiscourse/client.py
@@ -93,11 +93,11 @@ class DiscourseClient(object):
         """
         return self._delete('/admin/users/{0}.json'.format(userid), **kwargs)
 
-    def users(self, filter=None, **kwargs):
-        if filter is None:
-            filter = 'active'
+    def users(self, flag=None, **kwargs):
+        if flag is None:
+            flag = 'active'
 
-        return self._get('/admin/users/list/{0}.json'.format(filter), **kwargs)
+        return self._get('/admin/users/list/{0}.json'.format(flag), **kwargs)
 
     def private_messages(self, username=None, **kwargs):
         if username is None:
